4e2fbc7bc4edc305b0f1b0728556775fd4462fc5
shizhao1100 <sz_p@outlook.com>
2020-01-28 04:00:32 +0800

    [add way]: 增加一个回溯解法

--- 401.二进制手表.js
+++ 401.二进制手表.js
@@ -24,7 +24,7 @@ var readBinaryWatch = function (num) {
       if (lightNum === num) {
         if (j >= 10) {
           result.push(i + ':' + j);
-        }else{
+        } else {
           result.push(i + ':0' + j);
         }
       }
@@ -32,5 +32,73 @@ var readBinaryWatch = function (num) {
   }
   return result;
 };
+
+/**
+ * @param {number} num
+ * @return {string[]}
+ * @way 回溯法
+ * @from self
+ * @description 回溯本质上是一个递归，传两个变量进去，
+ * 一个维护当前值的状态，用于记录和返回最终状态，
+ * 一个维护当前进行到哪一步，用于边界判断，执行到哪一步之后退出
+ * 每个步骤 修改当前值记录状态，进入下一个状态。
+ * 退出时回溯当前状态。
+ * 
+ * 由于 时间为 小时为 0-11 分钟为 0-59 所以还需要对结果进行剪枝 
+ * 
+ * @result Accepted
+ * @O Cm n
+ * @time 2020-01-28 02:14:20
+ */
+
+var readBinaryWatch = function (num) {
+  let timeStatus = [];
+  let results = [];
+  // start 记录当前进行状态 与 num 进行比对判断边界
+  // timeStatusNow 用于记录当前值状态
+  function getTimeStatus(num, start, timeStatusNow) {
+    if (num === 0) {
+      timeStatus.push([].concat(timeStatusNow));
+      return;
+    }
+    for (let i = start; i < 10; i++) {
+      timeStatusNow[i] = true;
+      // 修改当前值 进入一个状态
+      getTimeStatus(num - 1, i + 1, timeStatusNow);
+      // 回溯当前值
+      timeStatusNow[i] = false;
+    }
+  }
+  function format(timeStatus) {
+    let hour = 0;
+    let time = 0;
+    for (let i = 0; i < 4; i++) {
+      if (timeStatus[i]) {
+        hour = hour + Math.pow(2, i);
+      }
+    }
+    for (let i = 4; i < timeStatus.length; i++) {
+      if (timeStatus[i]) {
+        time = time + Math.pow(2, i - 4);
+      }
+    }
+    if (time < 10) {
+      time = '0' + time;
+    }
+    // TODO 在这里做的异常值判断 并不是真是的剪枝操作。所以有一定的空间浪费
+    if (time > 59 || hour > 11) {
+      return undefined;
+    }
+    return `${hour}:${time}`;
+  }
+
+  getTimeStatus(num, 0, [false, false, false, false, false, false, false, false, false, false])
+  for (let i = 0; i < timeStatus.length; i++) {
+    let result = format(timeStatus[i]);
+    if (result !== undefined)
+      results.push(result);
+  }
+  return results;
+}
   // @lc code=end
 
