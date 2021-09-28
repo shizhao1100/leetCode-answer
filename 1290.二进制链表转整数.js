0deb68ba23515baf439a9f069b2a98540b9eff33
sz-p <SZ_P@outlook.com>
2021-09-28 22:58:19 +0800

    简单 字符串

---
+++ 1290.二进制链表转整数.js
@@ -0,0 +1,27 @@
+/*
+ * @lc app=leetcode.cn id=1290 lang=javascript
+ *
+ * [1290] 二进制链表转整数
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {number}
+ */
+var getDecimalValue = function (head) {
+  let stringNum = "";
+  while (head) {
+    stringNum = stringNum + head.val.toString();
+    head = head.next;
+  }
+  return parseInt(stringNum, 2);
+};
+// @lc code=end
