eb46f1e6abede8953875c801d6a2843f5abc86e9
sz-p <SZ_P@outlook.com>
2021-11-07 18:55:56 +0800

    简答 API

---
+++ 1185.一周中的第几天.js
@@ -0,0 +1,33 @@
+/*
+ * @lc app=leetcode.cn id=1185 lang=javascript
+ *
+ * [1185] 一周中的第几天
+ */
+
+// @lc code=start
+/**
+ * @param {number} day
+ * @param {number} month
+ * @param {number} year
+ * @return {string}
+ */
+var dayOfTheWeek = function (day, month, year) {
+  const dateDay = new Date(`${year}-${month}-${day}`).getDay();
+  switch (dateDay) {
+    case 1:
+      return "Monday";
+    case 2:
+      return "Tuesday";
+    case 3:
+      return "Wednesday";
+    case 4:
+      return "Thursday";
+    case 5:
+      return "Friday";
+    case 6:
+      return "Saturday";
+    case 0:
+      return "Sunday";
+  }
+};
+// @lc code=end
